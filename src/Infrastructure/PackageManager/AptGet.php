99bc540b15be6f99e8e597e38a1ee7f2db9aeb83
Herberto Graca <herberto.graca@gmail.com>
2017-09-08 00:03:44 +0200

    improuve apt-get install command so it gives feedback and actually works

--- src/Infrastructure/PackageManager/AptGet.php
+++ src/Infrastructure/PackageManager/AptGet.php
@@ -7,7 +7,7 @@ use Hgraca\Helper\StringHelper;
 final class AptGet implements PackageManagerInterface
 {
     const NAME = 'apt-get';
-    private const CMD_INSTALL = 'apt-get -y install php-xdebug';
+    private const CMD_INSTALL = 'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install php-xdebug';
     private const CMD_UNINSTALL = 'apt-get -y purge php-xdebug';
     private const CMD_IS_INSTALLED_MATCH = 'install ok installed';
     private const CMD_IS_INSTALLED = 'dpkg-query -W --showformat=\'${Status}\n\' php-xdebug | grep "'
@@ -16,12 +16,12 @@ final class AptGet implements PackageManagerInterface
 
     public function install(): void
     {
-        exec(self::CMD_INSTALL);
+        system(self::CMD_INSTALL);
     }
 
     public function uninstall(): void
     {
-        exec(self::CMD_UNINSTALL);
+        system(self::CMD_UNINSTALL);
     }
 
     public function isInstalled(): bool
